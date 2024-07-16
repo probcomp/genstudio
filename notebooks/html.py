d819a4baf5078010898041c2a58cfd9806b5331d
Matthew Huebert <mhuebert@gmail.com>
2024-07-16 11:35:32 -0600

    save any LayoutItem as html or image

--- notebooks/html.py
+++
@@ -1,6 +0,0 @@
-import genstudio.plot as Plot
-
-#
-(p := Plot.dot([[1, 1], [2, 2], [3, 1], [4, 2]]).html())
-#
-p.save("foo.html")
--- src/genstudio/widget.py
+++ src/genstudio/widget.py
@@ -1,10 +1,11 @@
 import datetime
 import json
-import uuid
+
 from typing import Iterable
 
 import anywidget
 import traitlets
+
 
 from genstudio.util import PARENT_PATH
 
@@ -30,63 +31,6 @@ def to_json(data):
     return json.dumps(data, default=default)
 
 
-def html_snippet(data, id):
-    serialized_data = to_json(data)
-
-    # Read and inline the JS and CSS files
-    with open(PARENT_PATH / "js/widget_build.js", "r") as js_file:
-        js_content = js_file.read()
-    with open(PARENT_PATH / "widget.css", "r") as css_file:
-        css_content = css_file.read()
-
-    html_content = f"""
-    <style>{css_content}</style>
-    <div id="{id}"></div>
-    <script type="module">
-        {js_content}
-        const container = document.getElementById('{id}');
-        const data = {serialized_data};
-        renderData(container, data);
-    </script>
-    """
-
-    return html_content
-
-
-def html_standalone(data, id):
-    return f"""
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <meta charset="UTF-8">
-        <title>GenStudio Widget</title>
-    </head>
-    <body>
-        {html_snippet(data, id)}
-    </body>
-    </html>
-    """
-
-
-def save_html(data, id, path):
-    html = html_standalone(data, id)
-    with open(path, "w") as f:
-        f.write(html)
-
-
-class HTML:
-    def __init__(self, data):
-        self.data = data
-        self.id = f"genstudio-widget-{uuid.uuid4().hex}"
-
-    def save(self, path):
-        save_html(self.data, self.id, path)
-
-    def _repr_mimebundle_(self, **kwargs):
-        html_content = html_snippet(self.data, self.id)
-        return {"text/html": html_content}, {}
-
-
 class Widget(anywidget.AnyWidget):
     _esm = PARENT_PATH / "js/widget_build.js"
     _css = PARENT_PATH / "widget.css"
@@ -98,7 +42,3 @@ class Widget(anywidget.AnyWidget):
 
     def _repr_mimebundle_(self, **kwargs):  # type: ignore
         return super()._repr_mimebundle_(**kwargs)
-
-    def html(self):
-        """Return an HTML representation of the widget."""
-        return HTML(self.data)
