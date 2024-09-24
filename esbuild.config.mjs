5f4cc1df17b890e37864a441084de3dca68a26f2
Matthew Huebert <mhuebert@gmail.com>
2024-09-24 15:26:24 +0200

    build: import map only required for cdn build.

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -5,15 +5,18 @@ import * as importMap from "esbuild-plugin-import-map";
 const args = process.argv.slice(2);
 const watch = args.includes('--watch');
 
-importMap.load('src/genstudio/js/import-map.cdn.json');
-
 const options = {
   entryPoints: ['src/genstudio/js/widget.js'],
   bundle: true,
   format: 'esm',
   outfile: 'src/genstudio/js/widget_build.js',
-  plugins: [cssModulesPlugin(), importMap.plugin()],
+  plugins: [cssModulesPlugin()],
 };
+
+if (watch) {
+  importMap.load('src/genstudio/js/import-map.cdn.json');
+  options.plugins.push(importMap.plugin());
+}
 
 const build = watch ? esbuild.context(options).then((r) => r.watch()) : esbuild.build(options)
 
