f241bb81452d7b2062dbe18b9b6883c472dba8b1
Matthew Huebert <mhuebert@gmail.com>
2025-01-31 15:35:50 +0100

    cleanup

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,7 +1,6 @@
 import esbuild from 'esbuild'
 import cssModulesPlugin from "esbuild-css-modules-plugin"
 import * as importMap from "esbuild-plugin-import-map";
-import {wgsl} from "@use-gpu/wgsl-loader/esbuild";
 
 const args = process.argv.slice(2);
 const watch = args.includes('--watch');
@@ -11,7 +10,7 @@ const options = {
   bundle: true,
   format: 'esm',
   outfile: 'src/genstudio/js/widget_build.js',
-  plugins: [wgsl(), cssModulesPlugin()],
+  plugins: [cssModulesPlugin()],
   minify: !watch,
   sourcemap: watch,
 };
